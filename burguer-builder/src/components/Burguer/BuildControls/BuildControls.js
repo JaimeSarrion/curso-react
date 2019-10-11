71e230b59a50c2ecdee3e0fb0182e21d99fb1781
Jaime Sarrion <jaime.sarrion@gmail.com>
2019-10-11 12:27:21 +0200

    Removing ingredients logic

--- burguer-builder/src/components/Burguer/BuildControls/BuildControls.js
+++ burguer-builder/src/components/Burguer/BuildControls/BuildControls.js
@@ -6,7 +6,7 @@ const controls = [
     { label: 'Salad', type: 'salad' },
     { label: 'Bacon', type: 'bacon' },
     { label: 'Cheese', type: 'cheese' },
-    { label: 'Meat', type: 'meat' }
+    { label: 'Meat', type: 'meat' } 
 ]
 
 const buildControls = (props) => (
@@ -17,6 +17,8 @@ const buildControls = (props) => (
                 key={ctrl.label}
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
+                removed = {()=> props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
             >
             </BuildControl>
         ))}
--- burguer-builder/src/containers/BurguerBuilder/BurguerBuilder.js
+++ burguer-builder/src/containers/BurguerBuilder/BurguerBuilder.js
@@ -35,10 +35,30 @@ class BurguerBuilder extends Component{
     }
 
     removeIngredientHandler = (type) =>{
-
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
     }
 
     render(){
+        const disableInfo = {
+            ...this.state.ingredients
+        }
+        for (let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0
+        }
+        // {salad: true, meat: false ...}
+
         return(
             <Aux>
                 <Burguer 
@@ -46,6 +66,8 @@ class BurguerBuilder extends Component{
                 ></Burguer>
                 <BuildControls
                     ingredientAdded= {this.addIngredientHandler}
+                    ingredientRemoved = {this.removeIngredientHandler}
+                    disabled={disableInfo}
                 ></BuildControls>
             </Aux>       
         );
