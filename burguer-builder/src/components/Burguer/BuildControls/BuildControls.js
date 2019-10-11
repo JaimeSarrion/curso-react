0f54ebbb0a88750b1ddc26493d06456597c4b918
Jaime Sarrion <jaime.sarrion@gmail.com>
2019-10-11 12:15:02 +0200

    Logic for adding ingredients

--- burguer-builder/src/components/Burguer/BuildControls/BuildControls.js
+++ burguer-builder/src/components/Burguer/BuildControls/BuildControls.js
@@ -3,25 +3,24 @@ import classes from './BuildControls.module.css'
 import BuildControl from './BuildControl/BuildControl'
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'}
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
 ]
 
 const buildControls = (props) => (
 
     <div className={classes.BuildControls}>
-        {controls.map((ctrl)=>(
+        {controls.map((ctrl) => (
             <BuildControl
                 key={ctrl.label}
                 label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
             >
-
             </BuildControl>
         ))}
     </div>
-
 )
 
 export default buildControls
