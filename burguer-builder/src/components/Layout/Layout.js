be3ae278ff3b1737f6c45991b75aab704ff02c09
Jaime Sarrion <jss72@alu.ua.es>
2019-10-14 10:27:18 +0200

    Hidding the side drawer

--- burguer-builder/src/components/Layout/Layout.js
+++ burguer-builder/src/components/Layout/Layout.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Auxiliarie'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 
-const layout = (props) => {
-    return (
-        <Aux>
-            <Toolbar></Toolbar>
-            <SideDrawer></SideDrawer>
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Aux>
-    )
+class Layout extends Component {
+    state = {
+        showSideDraw: true
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({ showSideDraw: false })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar></Toolbar>
+                <SideDrawer
+                    open={this.state.showSideDraw}
+                    closed={this.sideDrawerCloseHandler}></SideDrawer>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+
 }
 
-export default layout
+export default Layout
--- burguer-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burguer-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,28 @@ import React from 'react'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import Logo from '../../Logo/Logo';
 import classes from './SideDrawer.module.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Auxiliarie';
 
 const sideDrawer = (props) => {
-    // ...
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className = {classes.Logo}>
-                <Logo></Logo>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}></Backdrop>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo></Logo>
+                </div>
+                <nav>
+                    <NavigationItems></NavigationItems>
+                </nav>
             </div>
-            <nav>
-                <NavigationItems></NavigationItems>
-            </nav>
-        </div>
+        </Aux>
+
     );
 }
 export default sideDrawer
