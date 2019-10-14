342fc46db954ef1d85098c58a4e1484cfcb59629
Jaime Sarrion <jss72@alu.ua.es>
2019-10-14 09:30:56 +0200

    Adding a menu for mobile devices

--- burguer-builder/src/components/Layout/Layout.js
+++ burguer-builder/src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Aux from '../../hoc/Auxiliarie'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+
 
 const layout = (props) => {
     return (
         <Aux>
             <Toolbar></Toolbar>
-
+            <SideDrawer></SideDrawer>
             <main className={classes.Content}>
                 {props.children}
             </main>
