756f79488c8f68cdbead3f9ffd959db56ffacfac
Jaime Sarrion <jss72@alu.ua.es>
2019-10-14 15:24:41 +0200

    Adding some axios call

--- Blog/src/containers/Blog/Blog.js
+++ Blog/src/containers/Blog/Blog.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-
+import Axios from 'axios'
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    state = {
+        post: []
+    }
+
+    componentDidMount() {
+        Axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                const post = response.data.slice(0,4)
+                const updatedPost = post.map(post => {
+                    return {
+                        ...post,
+                        author: 'Jaime'
+                    }
+                })
+
+                this.setState({ post: updatedPost })
+            })
+    }
+
+    render() {
+        const posts = this.state.post.map((post) => {
+            return <Post 
+                key={post.id} 
+                title={post.title}
+                author= {post.author} />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
