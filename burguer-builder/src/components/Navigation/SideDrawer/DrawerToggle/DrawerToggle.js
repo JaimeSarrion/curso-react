3add4db0976ada5ac9ddce5e64a69a2c01ca8837
Jaime Sarrion <jss72@alu.ua.es>
2019-10-14 11:01:10 +0200

    Adding a icon for the menu button

--- burguer-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ burguer-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import classes from './DrawerToggle.module.css'
 const DrawerToggle = (props) => (
     <div 
         onClick={props.clicked}
-    >MENU
+        className= {classes.DrawerToggle}
+        >
+            <div></div>
+            <div></div>
+            <div></div>
+
     </div>
 )
 
