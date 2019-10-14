0d8471c445ed5d3d72fbb611257b65881b0a2c64
Jaime Sarrion <jss72@alu.ua.es>
2019-10-14 09:18:55 +0200

    Adding navigation bar

--- burguer-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burguer-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
-
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) =>(
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo></Logo>
-        <nav>...</nav>
+        <nav>
+            <NavigationItems></NavigationItems>
+        </nav>
     </header>
 );
 
