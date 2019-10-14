3212066294844a9ff22ae32b1db2236a1ef291bd
Jaime Sarrion <jss72@alu.ua.es>
2019-10-14 15:49:24 +0200

    Adding logic to do a post mehtod with axios

--- Blog/src/components/NewPost/NewPost.js
+++ Blog/src/components/NewPost/NewPost.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './NewPost.css';
+import Axios from 'axios';
 
 class NewPost extends Component {
     state = {
@@ -8,7 +9,17 @@ class NewPost extends Component {
         content: '',
         author: 'Max'
     }
-
+    postDataHandler = () => {
+        const data ={
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+        Axios.post('https://jsonplaceholder.typicode.com/posts', data)
+        .then((response)=>{
+            console.log(response)
+        })
+    }
     render () {
         return (
             <div className="NewPost">
@@ -22,7 +33,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
